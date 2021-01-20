605cf4a8f2c084d2b97a374c473bb948700900d4
Junus Ergin <junus@developerakademie.com>
2021-01-20 16:28:07 +0100

    legal sides

--- src/app/legal/cookie/cookie.component.ts
+++ src/app/legal/cookie/cookie.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cookie',
   templateUrl: './cookie.component.html',
   styleUrls: ['./cookie.component.scss']
 })
-export class CookieComponent implements OnInit {
+export class CookieComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://consent.cookiebot.com/e2564c46-20a0-42c7-b51d-b7172eadc5de/cd.js';
+    document.getElementById('my-cookie-section').appendChild(s);
   }
 
 }
